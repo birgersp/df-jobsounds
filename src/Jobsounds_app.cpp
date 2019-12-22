073d64396429e4a645a61c446b125c28cc0496ee
Birger Skogeng Pedersen <birger.sp@gmail.com>
2019-12-22 19:30:46 +0100

    add app class

---
+++ src/Jobsounds_app.cpp
@@ -0,0 +1,13 @@
+#include "Jobsounds_app.h"
+
+void Jobsounds_app::run(const Vector<String>& arguments)
+{
+	for (StringRef argument : arguments)
+		parse_argument(argument);
+	server.bind(port);
+}
+
+void Jobsounds_app::parse_argument(String_Ref argument)
+{
+
+}
---
+++ src/Jobsounds_app.h
@@ -0,0 +1,21 @@
+#ifndef JOBSOUNDS_APP_H
+#define JOBSOUNDS_APP_H
+
+#include "core.h"
+#include "Socket_Server.h"
+
+class Jobsounds_app
+{
+public:
+
+	void run(const Vector<String>& arguments);
+
+private:
+
+	uint port = 56730;
+	Socket_Server server;
+	void parse_argument(String_Ref argument);
+
+};
+
+#endif /* JOBSOUNDS_APP_H */
