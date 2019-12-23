552fce6e14d7ee042741eafdee8f51e99923e995
Birger Skogeng Pedersen <birger.sp@gmail.com>
2019-12-23 22:46:10 +0100

    implement parse_dwarf_job_msg

--- src/Dwarf_job_msg.cpp
+++ src/Dwarf_job_msg.cpp
@@ -5,10 +5,13 @@
 
 #include "Dwarf_job_msg.h"
 #include <cpputil/Stringreader.hpp>
+#include <cpputil/string.hpp>
 
 Dwarf_job_msg parse_dwarf_job_msg(String_ref string)
 {
-	throw unsupported_function_exception();
+	Vector<String> split = cpputil::split_string(string, ' ');
 	Dwarf_job_msg message;
+	message.dwarf_id = std::stoi(split[0]);
+	message.job_id = std::stoi(split[1]);
 	return message;
 }
