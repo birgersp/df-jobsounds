47e6c23788e1eaf6e262b74c7b6b6e6ffae2f67b
Birger Skogeng Pedersen <birger.sp@gmail.com>
2019-12-26 15:18:00 +0100

    remove camelcase in lua script

--- lua/jobsounds.lua
+++ lua/jobsounds.lua
@@ -6,69 +6,69 @@ Job sounds
 ===========
 Connects to a socket (tcp), checks the current job of dwarves and sends messages
 
-]====] local scriptName = 'jobsounds'
+]====] local script_name = 'jobsounds'
 
-local function initSocket() socket = tcp:connect('127.0.0.1', 56730) end
+local function init_socket() socket = tcp:connect('127.0.0.1', 56730) end
 
 local function start()
     luasocket = require("plugins.luasocket")
     tcp = luasocket.tcp
-    dfhack.println(scriptName .. " connecting...")
+    dfhack.println(script_name .. " connecting...")
     socket = nil
-    pcall(initSocket)
+    pcall(init_socket)
     if (socket ~= nil) then
-        dfhack.println(scriptName .. " connected")
+        dfhack.println(script_name .. " connected")
         stop = false
-        timeLastSend = os.time()
+        time_last_send = os.time()
         loop()
     else
-        dfhack.println(scriptName .. " could not connect")
+        dfhack.println(script_name .. " could not connect")
     end
 end
 
 local function send(msg)
     if (pcall(socket.send, socket, msg) == true) then
-        timeLastSend = os.time()
+        time_last_send = os.time()
     else
         stop = true
     end
 end
 
-local function triggerJobSound(unit, jobType)
+local function trigger_job_sound(unit, job_type)
     if (socket ~= nil) then
         if (df.global.window_z == unit.pos.z and df.global.window_x < unit.pos.x and
             df.global.window_x + 90 > unit.pos.x and df.global.window_y <
             unit.pos.y and df.global.window_y + 60 > unit.pos.y) then
-            send(unit.id .. " " .. jobType .. "\0")
+            send(unit.id .. " " .. job_type .. "\0")
         end
     end
 end
 
-local function handleDwarf(unit)
+local function handle_dwarf(unit)
     if (unit.job.current_job ~= nil) then
-        local jobPos = unit.job.current_job.pos
-        local dx = math.abs(jobPos.x - unit.pos.x)
-        local dy = math.abs(jobPos.y - unit.pos.y)
-        local dz = math.abs(jobPos.z - unit.pos.z)
+        local job_pos = unit.job.current_job.pos
+        local dx = math.abs(job_pos.x - unit.pos.x)
+        local dy = math.abs(job_pos.y - unit.pos.y)
+        local dz = math.abs(job_pos.z - unit.pos.z)
         if (dx <= 1 and dy <= 1 and dz <= 1) then
-            triggerJobSound(unit, unit.job.current_job.job_type)
+            trigger_job_sound(unit, unit.job.current_job.job_type)
         end
     end
 end
 
-local function checkDwarves()
+local function check_dwarves()
     for k, unit in ipairs(df.global.world.units.active) do
-        if (dfhack.units.isDwarf(unit)) then handleDwarf(unit) end
+        if (dfhack.units.isDwarf(unit)) then handle_dwarf(unit) end
     end
 end
 
 function loop()
     if (stop == true) then
-        dfhack.println(scriptName .. " stopped")
+        dfhack.println(script_name .. " stopped")
         return
     end
 
-    if (df.global.pause_state == false) then checkDwarves() end
+    if (df.global.pause_state == false) then check_dwarves() end
 
     dfhack.timeout(15, 'ticks', loop)
 end
